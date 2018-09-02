307b4e783efda5420542b03f7e6e1d7fd498a8ee
Marlon Monzon <martianworm17@gmail.com>
2018-09-02 09:44:04 +0800

    #4.2 Kotlin Data Types: VAR vs VAL: Variables and Constants Part-1

--- src/myfirst.kt
+++ src/myfirst.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    // REPL Read Eval Print Loop
+
+    var age: Int = 10
+    var age2 = 10 // It becomes Integer automatically
+
+    var name: String = "Hands"
+    var name2 = "Hands" // It becomes String automatically
+
+    var isAlive: Boolean = true
+    var isAlive2 = true // It becomes Boolean automatically
+
+    // Data Types in Kotlin: All are Objects
 }
